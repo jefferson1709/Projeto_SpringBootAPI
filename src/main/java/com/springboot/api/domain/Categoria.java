f20e0b2b08f91697c834af678c1d0769b1e9a2af
Jefferson dos santos <Jefferson1709@hotmail.com>
2021-10-14 20:57:11 -0300

    Criando repository e service para categoria

--- src/main/java/com/springboot/api/domain/Categoria.java
+++ src/main/java/com/springboot/api/domain/Categoria.java
@@ -1,11 +1,17 @@
 package com.springboot.api.domain;
 
 import java.io.Serializable;
-import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
@@ -36,7 +42,10 @@ public class Categoria implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -48,7 +57,12 @@ public class Categoria implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Categoria other = (Categoria) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 	
