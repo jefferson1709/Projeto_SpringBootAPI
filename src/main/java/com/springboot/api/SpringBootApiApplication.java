287d28351052335950f3db445d5f57a4ecb3037c
Jefferson dos santos <Jefferson1709@hotmail.com>
2021-10-14 22:27:53 -0300

    Cliente, TipoCliente, Telefone e endereço

--- src/main/java/com/springboot/api/SpringBootApiApplication.java
+++ src/main/java/com/springboot/api/SpringBootApiApplication.java
@@ -8,10 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.springboot.api.domain.Categoria;
 import com.springboot.api.domain.Cidade;
+import com.springboot.api.domain.Cliente;
+import com.springboot.api.domain.Endereco;
 import com.springboot.api.domain.Estado;
 import com.springboot.api.domain.Produto;
+import com.springboot.api.domain.enums.TipoCliente;
 import com.springboot.api.repositories.CategoriaRepository;
 import com.springboot.api.repositories.CidadeRepository;
+import com.springboot.api.repositories.ClienteRepository;
+import com.springboot.api.repositories.EnderecoRepository;
 import com.springboot.api.repositories.EstadoRepository;
 import com.springboot.api.repositories.ProdutoRepository;
 
@@ -26,6 +31,10 @@ public class SpringBootApiApplication implements CommandLineRunner {
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApiApplication.class, args);
@@ -50,7 +59,7 @@ public class SpringBootApiApplication implements CommandLineRunner {
 				
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-		
+
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
 		
@@ -63,6 +72,18 @@ public class SpringBootApiApplication implements CommandLineRunner {
 
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null, "Jefferson", "jefferson@teste.com", "36378912377", TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("123456789", "1234567898"));
+		
+		Endereco e1 = new Endereco(null, "Rua aprovada", "999", "Apto 333", "Centro", "1234567", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida confirmada", "333", "Sala 999", "Lado", "1234568", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 	}
 
 }
