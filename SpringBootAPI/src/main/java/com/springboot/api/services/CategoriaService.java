a3b2f0dbb63d7d4d081ec4d770ab03d3b1fa03ca
Jefferson dos santos <Jefferson1709@hotmail.com>
2021-10-15 20:15:01 -0300

    Paginacao com parametros opcionais na requisicao

--- SpringBootAPI/src/main/java/com/springboot/api/services/CategoriaService.java
+++ SpringBootAPI/src/main/java/com/springboot/api/services/CategoriaService.java
@@ -1,9 +1,13 @@
 package com.springboot.api.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.springboot.api.domain.Categoria;
@@ -41,5 +45,14 @@ public class CategoriaService {
 		catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
+	}
+	
+	public List<Categoria> buscartudo() {
+		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
