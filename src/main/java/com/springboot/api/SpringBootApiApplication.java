8a727dd9abc9becd68db0d7040867bd49734ad6f
Jefferson dos santos <Jefferson1709@hotmail.com>
2021-10-14 22:07:46 -0300

    Add Estado e Cidade

--- src/main/java/com/springboot/api/SpringBootApiApplication.java
+++ src/main/java/com/springboot/api/SpringBootApiApplication.java
@@ -7,8 +7,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.springboot.api.domain.Categoria;
+import com.springboot.api.domain.Cidade;
+import com.springboot.api.domain.Estado;
 import com.springboot.api.domain.Produto;
 import com.springboot.api.repositories.CategoriaRepository;
+import com.springboot.api.repositories.CidadeRepository;
+import com.springboot.api.repositories.EstadoRepository;
 import com.springboot.api.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -18,6 +22,10 @@ public class SpringBootApiApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApiApplication.class, args);
@@ -42,6 +50,19 @@ public class SpringBootApiApplication implements CommandLineRunner {
 				
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
