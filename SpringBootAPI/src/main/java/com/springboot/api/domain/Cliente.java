f25f91114a9ce619a64b30c637faf87004b88209
Jefferson dos santos <Jefferson1709@hotmail.com>
2021-10-16 11:01:19 -0300

    PUT, DELETE e GET para Cliente

--- SpringBootAPI/src/main/java/com/springboot/api/domain/Cliente.java
+++ SpringBootAPI/src/main/java/com/springboot/api/domain/Cliente.java
@@ -51,6 +51,7 @@ public class Cliente implements Serializable {
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipo = tipo.getCod();
+		this.tipo = (tipo==null) ? null : tipo.getCod();
 	}
 
 	public Integer getId() {
