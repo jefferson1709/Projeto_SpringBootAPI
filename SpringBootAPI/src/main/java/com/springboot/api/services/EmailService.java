beea95e647dcca7a6323becd7ba9d287e82d01b4
Jefferson dos santos <Jefferson1709@hotmail.com>
2021-10-17 20:17:04 -0300

    Refresh token e esqueci a senha

--- SpringBootAPI/src/main/java/com/springboot/api/services/EmailService.java
+++ SpringBootAPI/src/main/java/com/springboot/api/services/EmailService.java
@@ -2,10 +2,12 @@ package com.springboot.api.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.springboot.api.domain.Cliente;
 import com.springboot.api.domain.Pedido;
 
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
 	void sendEmail(SimpleMailMessage msg);
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
