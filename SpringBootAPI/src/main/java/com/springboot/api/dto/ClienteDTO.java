ad38e6037d623b980ea785056a3c3d94eb73fdef
Jefferson dos santos <Jefferson1709@hotmail.com>
2021-10-17 14:54:08 -0300

    Inserindo Pedido

--- SpringBootAPI/src/main/java/com/springboot/api/dto/ClienteDTO.java
+++ SpringBootAPI/src/main/java/com/springboot/api/dto/ClienteDTO.java
@@ -8,8 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.springboot.api.domain.Cliente;
+import com.springboot.api.services.validation.ClienteUpdate;
 
-
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
--- SpringBootAPI/src/main/java/com/springboot/api/services/PedidoService.java
+++ SpringBootAPI/src/main/java/com/springboot/api/services/PedidoService.java
@@ -1,12 +1,19 @@
 package com.springboot.api.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.springboot.api.domain.ItemPedido;
+import com.springboot.api.domain.PagamentoComBoleto;
 import com.springboot.api.domain.Pedido;
+import com.springboot.api.domain.enums.EstadoPagamento;
+import com.springboot.api.repositories.ItemPedidoRepository;
+import com.springboot.api.repositories.PagamentoRepository;
 import com.springboot.api.repositories.PedidoRepository;
+import com.springboot.api.repositories.ProdutoRepository;
 import com.springboot.api.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -14,11 +21,39 @@ public class PedidoService {
 	
 	@Autowired
 	private PedidoRepository repo;
+	@Autowired
+	private BoletoService boletoService;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public Pedido buscar(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(ip.getProduto().getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 
 }
