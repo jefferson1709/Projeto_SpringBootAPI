4bb018e1776cd00e0c6884328fcd701c18201aeb
Jefferson dos santos <Jefferson1709@hotmail.com>
2021-10-17 18:12:00 -0300

    Implementando autorizacao

--- SpringBootAPI/src/main/java/com/springboot/api/config/SecurityConfig.java
+++ SpringBootAPI/src/main/java/com/springboot/api/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.springboot.api.security.JWTAuthenticationFilter;
+import com.springboot.api.security.JWTAuthorizationFilter;
 import com.springboot.api.security.JWTUtil;
 
 @Configuration
@@ -54,6 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
