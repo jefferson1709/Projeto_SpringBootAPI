c55794025d211c33484a2acaff2abfb69d7a476e
Jefferson dos santos <Jefferson1709@hotmail.com>
2021-10-17 16:23:40 -0300

    Implementando toString de Pedido

--- SpringBootAPI/src/main/java/com/springboot/api/services/PedidoService.java
+++ SpringBootAPI/src/main/java/com/springboot/api/services/PedidoService.java
@@ -19,13 +19,17 @@ import com.springboot.api.services.exceptions.ObjectNotFoundException;
 public class PedidoService {
 	
 	@Autowired
-	private PedidoRepository repo;
+	private PedidoRepository repo;	
 	@Autowired
 	private BoletoService boletoService;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	@Autowired
+	private ProdutoService produtoService;
+	@Autowired
+	private ClienteService clienteService;
 	
 	public Pedido buscar(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
@@ -36,6 +40,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.buscar(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -46,10 +51,12 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
 			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 
